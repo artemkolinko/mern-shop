091d5905c9fe7e078f623a36656e6a24fcd2083c
Artem Kolinko <kalinart@gmail.com>
2021-02-22 16:27:53 +0200

    Add loader and alert message

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -18,21 +18,3 @@ const store = createStore(
 );
 
 export default store;
-
-// import { createStore, combineReducers, applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-
-// const reducer = combineReducers({})
-
-// const initialState = {}
-
-// const middleware = [thunk]
-
-// const store = createStore(
-//   reducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// )
-
-// export default store
